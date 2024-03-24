a0d6f913a5165ef5f9e44bc414c893294ab60e63
vipulp93 <edupurpose288@gmail.com>
2024-03-24 18:50:27 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,11 @@
+// const heading = React.createElement("h1",{id: "heading"},"Hello World From React!");
+//         const root = ReactDOM.createRoot(document.getElementById("root"));
+//         root.render(heading);
+
+
+const parent = React.createElement("div", {id : "parent"},
+                React.createElement("div", {id : "child"},
+                [React.createElement("h1",{},"I am a h1 tag"),React.createElement("h2",{},"I am a h2 tag")]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent);
